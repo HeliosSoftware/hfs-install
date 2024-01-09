ec735f1407c9a747144cc21b39fc558df4783708
Steve Munini <smunini@gmail.com>
2024-01-09 16:58:28 -0500

    Moved AxonOps org name and access key to environment variables.

--- ansible/apache-cassandra-axonops.sh
+++ ansible/apache-cassandra-axonops.sh
@@ -1,3 +1,4 @@
 source ../.aws/config
+export ANSIBLE_HOST_KEY_CHECKING=false
 cp -r group_vars/tag_ClusterName_axonops group_vars/tag_ClusterName_$TF_VAR_CASSANDRA_CLUSTER_NAME
 ansible-playbook -i inventory/axonops/aws_ec2.yml apache-cassandra-axonops.yml --diff
--- ansible/start-cassandra.sh
+++ ansible/start-cassandra.sh
@@ -1,2 +1,3 @@
 source ../.aws/config
+export ANSIBLE_HOST_KEY_CHECKING=false
 ansible-playbook -i inventory/axonops/aws_ec2.yml start-cassandra.yml --diff
