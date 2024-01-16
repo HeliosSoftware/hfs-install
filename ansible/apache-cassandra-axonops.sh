5d4fe2783d1ac5b94de512e6a0ca95045815376d
Steve Munini <smunini@gmail.com>
2024-01-16 13:58:43 -0500

    Consolidated AWS_DEFAULT_REGION

--- ansible/apache-cassandra-axonops.sh
+++ ansible/apache-cassandra-axonops.sh
@@ -1,4 +1,5 @@
 source ../.aws/config
 export ANSIBLE_HOST_KEY_CHECKING=false
+export TF_VAR_AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
 cp -r group_vars/tag_ClusterName_axonops group_vars/tag_ClusterName_$TF_VAR_CASSANDRA_CLUSTER_NAME
 ansible-playbook -i inventory/axonops/aws_ec2.yml apache-cassandra-axonops.yml --diff
--- ansible/start-cassandra.sh
+++ ansible/start-cassandra.sh
@@ -1,3 +1,4 @@
 source ../.aws/config
 export ANSIBLE_HOST_KEY_CHECKING=false
+export TF_VAR_AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
 ansible-playbook -i inventory/axonops/aws_ec2.yml start-cassandra.yml --diff
