dfa4f375d1b6e2f6a4c703b2e27458cc48af503b
Steve Munini <smunini@gmail.com>
2024-02-29 08:07:33 -0500

    Added test_import_server1

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -43,46 +43,7 @@ resource "aws_instance" "bastion_server" {
   associate_public_ip_address = true
   key_name                    = aws_key_pair.helios_local_key_pair.key_name
   ebs_optimized               = true
-  root_block_device {
-    delete_on_termination = true
-    encrypted = true
-    volume_size = 20
-    volume_type = "gp3"
-  }
-  ephemeral_block_device {
-    device_name = "/dev/sdc"
-    virtual_name = "ephemeral0"
-  }
   depends_on                  = [aws_instance.cassandra_0, aws_instance.cassandra_1, aws_instance.cassandra_2]
-//  user_data = <<EOF
-//#!/bin/bash
-//export DEBIAN_FRONTEND=noninteractive
-//sudo apt-get update
-//sudo apt-get -y install python3-pip
-//sudo apt-get -y install pipenv
-//sudo apt-add-repository -y ppa:ansible/ansible
-//sudo apt --yes update
-//sudo apt --yes install ansible
-//sudo pip3 install boto3
-//sudo pip3 install ansible
-//sudo apt --yes install unzip
-//curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-//unzip awscliv2.zip
-//sudo ./aws/install
-//curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.28.3/2023-11-14/bin/linux/amd64/kubectl
-//chmod +x ./kubectl
-//mkdir -p /home/ubuntu/bin && cp ./kubectl /home/ubuntu/bin/kubectl && chown ubuntu:ubuntu /home/ubuntu/bin && chown ubuntu:ubuntu /home/ubuntu/bin/kubectl
-//echo 'export PATH=/home/ubuntu/bin:$PATH' >> /home/ubuntu/.bashrc
-//echo 'aws eks update-kubeconfig --region ${var.AWS_DEFAULT_REGION} --name ${aws_eks_cluster.helios-eks-cluster.name}' >> /home/ubuntu/.bashrc
-//git clone https://github.com/HeliosSoftware/hfs-install.git /home/ubuntu/hfs-install
-//mkdir /home/ubuntu/hfs-install/.aws
-//chown -R ubuntu:ubuntu /home/ubuntu/hfs-install
-//echo '${tls_private_key.ssh.private_key_openssh}' >> /home/ubuntu/.ssh/id_ed25519
-//echo '${tls_private_key.ssh.public_key_openssh}' >> /home/ubuntu/.ssh/id_ed25519.pub
-//chown ubuntu:ubuntu /home/ubuntu/.ssh/id_ed25519
-//chown ubuntu:ubuntu /home/ubuntu/.ssh/id_ed25519.pub
-//chmod 400 /home/ubuntu/.ssh/id_ed25519
-//  EOF
   source_dest_check = false
   lifecycle {
     ignore_changes = [
@@ -117,10 +78,7 @@ resource "aws_instance" "bastion_server" {
       "echo '${tls_private_key.ssh.public_key_openssh}' >> /home/ubuntu/.ssh/id_ed25519.pub",
       "chown ubuntu:ubuntu /home/ubuntu/.ssh/id_ed25519",
       "chown ubuntu:ubuntu /home/ubuntu/.ssh/id_ed25519.pub",
-      "chmod 400 /home/ubuntu/.ssh/id_ed25519",
-      "sudo apt-get install java-common",
-      "wget https://corretto.aws/downloads/latest/amazon-corretto-17-x64-linux-jdk.deb",
-      "sudo dpkg --install java-17-amazon-corretto-jdk_17.0.0.35-1_amd64.deb"
+      "chmod 400 /home/ubuntu/.ssh/id_ed25519"
     ]
     connection {
       type = "ssh"
@@ -378,4 +336,61 @@ resource "aws_instance" "cassandra_2" {
 //  }
 //}
 
-
+# Bastion Gateway Server
+resource "aws_instance" "test_import_server1" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.small"
+  tags = {
+    Name        = "Test Import Server 1"
+    Role        = "gateway"
+    Environment = var.ENVIRONMENT
+  }
+  availability_zone           = var.zone_1
+  subnet_id                   = aws_subnet.public-subnet-1.id
+  vpc_security_group_ids      = [aws_security_group.allow_outbound_all.id, aws_security_group.allow_tls.id]
+  private_ip                  = "10.0.1.9"
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.helios_local_key_pair.key_name
+  ebs_optimized               = true
+  root_block_device {
+    delete_on_termination = true
+    encrypted = true
+    volume_size = 20
+    volume_type = "gp3"
+  }
+  ephemeral_block_device {
+    device_name = "/dev/sdc"
+    virtual_name = "ephemeral0"
+  }
+  depends_on                  = [aws_instance.cassandra_0, aws_instance.cassandra_1, aws_instance.cassandra_2]
+  source_dest_check = false
+  lifecycle {
+    ignore_changes = [
+      ami
+    ]
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "export DEBIAN_FRONTEND=noninteractive",
+      "sudo apt-get update",
+      "echo '${tls_private_key.ssh.private_key_openssh}' >> /home/ubuntu/.ssh/id_ed25519",
+      "echo '${tls_private_key.ssh.public_key_openssh}' >> /home/ubuntu/.ssh/id_ed25519.pub",
+      "chown ubuntu:ubuntu /home/ubuntu/.ssh/id_ed25519",
+      "chown ubuntu:ubuntu /home/ubuntu/.ssh/id_ed25519.pub",
+      "chmod 400 /home/ubuntu/.ssh/id_ed25519",
+      "wget -O - https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /usr/share/keyrings/corretto-keyring.gpg && echo 'deb [signed-by=/usr/share/keyrings/corretto-keyring.gpg] https://apt.corretto.aws stable main' | sudo tee /etc/apt/sources.list.d/corretto.list",
+      "sudo apt-get update; sudo apt-get install -y java-11-amazon-corretto-jdk",
+      "git clone https://github.com/synthetichealth/synthea.git",
+      "git clone https://github.com/HeliosSoftware/fhir-importer.git",
+      "cd fhir-importer",
+      "sudo apt -y install maven",
+      "mvn clean install",
+    ]
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      host = self.public_ip
+      private_key = file(var.local_ssh_private_key)
+    }
+  }
+}
